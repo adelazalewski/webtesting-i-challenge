21d3d1f6293ea679b83a7609cedeaa3c9c89ab1c
adelazalewski <paliuadela@yahoo.com>
2021-01-19 17:53:54 -0500

    finished success and repair plus testing working on fail tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,40 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancement < 0) {
+    throw new Error ("item.enhancement cant be less then 0")
+  }
+  
+  if(item.enhancement === 20){
+    return { ...item}  
+  }
+  if(item.enhancement < 20) {
+    return {...item, enhancement: item.enhancement + 1}
+  }
+  
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 0) {
+    throw new Error ("item.enhancement cant be less then 0")
+  }
+  if(0 >= item.enhancement < 15) {
+    return {...item, durability: item.durability - 5}
+  }
+  if(item.enhancement === 15 || item.enhancement === 16) {
+    return {...item, durability: item.durability - 10}
+  }
+  if(item.enhancement === 17 || item.enhancement === 18) {
+    return {...item, enhancement: item.enhancement - 1}
+  }
+  
+  
 }
 
 function repair(item) {
-  return { ...item };
+  if(item.durability <= 100){
+  return { ...item, durability: 100 };
+  }
 }
 
 function get(item) {
